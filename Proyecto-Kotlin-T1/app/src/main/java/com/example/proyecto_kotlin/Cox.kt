4f6cb913a781f7d506b32f853a3074ba9e94d429
Sui <149364828+PabloGHub@users.noreply.github.com>
2024-11-26 10:13:35 +0100

    Ya puedes conectarte

--- Proyecto-Kotlin-T1/app/src/main/java/com/example/proyecto_kotlin/Cox.kt
+++ Proyecto-Kotlin-T1/app/src/main/java/com/example/proyecto_kotlin/Cox.kt
@@ -1,25 +1,37 @@
 package com.example.proyecto_kotlin
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
 
 class Cox
 (
-    _host:      String,
-    _puerto:    String,
-    _usu:       String,
-    _contra:    String,
-    _BD:        String
+    private val _host:      String,
+    private val _puerto:    String,
+    private val _usu:       String,
+    private val _contra:    String,
+    private val _BD:        String
 )
 {
-    lateinit var _host: String;
-    lateinit var _puerto: String;
-    lateinit var _usu: String;
-    lateinit var _contra: String;
-    lateinit var _BD: String;
+    private var _url: String = "jdbc:postgresql://$_host:$_puerto/$_BD";
 
-    // conec-tar
-    fun conec(): Boolean
+    fun conectar(): Connection?
     {
+        return try
+        {
+            Class.forName("org.postgresql.Driver");
+            DriverManager.getConnection(_url, _usu, _contra);
+        }
+        catch (_cnfe: ClassNotFoundException)
+        {
+            println("Error en Drivers de Postgres: ${_cnfe.message}");
+            null;
+        }
+        catch (_sqle: SQLException)
+        {
+            println("Error al conectarse a la BD: ${_sqle.message}");
+            null;
+        }
 
-        return false;
     }
 
 }
